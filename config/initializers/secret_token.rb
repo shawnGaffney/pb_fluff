0511cfa9b40a4778f8c144cd1158c27071b36b74
Shawn <gaffneyshawn23@gmail.com>
2011-10-13 21:44:39 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PbFluff::Application.config.secret_token = 'd9e7dd3e7022f2b3366ba284247235ec5eeddeec39444b8b3b9669a8549e9e486db8eeaebd864c7d40824199bed36dd4641476c12226dab1967080b138af6c31'
